2d96bdb75c4e83f31affab8a72872c6785d7a2e9
Steve Kuzanwa <stevekuzanwa@outlook.com>
2019-08-13 20:57:53 +0200

    Guykan Car Wash

---
+++ js/queryloader/queryloader.js
@@ -0,0 +1,11 @@
+window.addEventListener('DOMContentLoaded', function() {
+    new QueryLoader2(document.querySelector("body"), {
+        barColor: "#fff",
+        backgroundColor: "#26543a",
+        barHeight: 2,
+        minimumTime: 500,
+        fadeOutTime: 1000
+    });
+});
+
+
